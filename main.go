2649c0469ada807e2fae4fa85adc967a80daf88c
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-04-22 22:15:53 -0400

    this is mindblowing

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+
+	myBook := &Book{
+		Name: "Animal Farm",
+		Isbn: 104,
+	}
+
+	data, err := proto.Marshal(myBook)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+
+	fmt.Println(data)
+
+	myNewBook := &Book{}
+	err = proto.Unmarshal(data, myNewBook)
+	if err != nil {
+		log.Fatal("Unmarshaling error: ", err)
+	}
+
+	fmt.Println(myNewBook.GetName())
+	fmt.Println(myNewBook.GetIsbn())
+}
